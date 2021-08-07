91b0418934081b04328c3a6484ebaaebc400fbd0
Molly Wilson <molly_wilson1@outlook.com>
2021-08-07 17:57:15 -0500

    Gallery styled

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,13 @@ const projects = require('../projects.json');
 
 
 function Gallery() {
+
     return (
-        projects.map(project => {
-            return <Project project={project} key={project.title}></Project>
-        })
+        <div className='row d-flex justify-content-center my-2'>
+            {projects.map(project => {
+                return <Project project={project} key={project.title}></Project>
+            })}
+        </div>
     )
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,34 @@
-function Project({project}) {
+function Project({ project }) {
 
     return (
-        <div className="flex-row">
-            {/* <img
-                src={require(project.image).default}
+        <div className="card mx-3 my-2">
+            <img
+                src={project.image}
                 alt={project.title}
-                className="img-thumbnail mx-1"
-                key={project.title}
-            /> */}
-            <h2>{project.title}</h2>
+                className="card-img-top"
+            />
+            <div className="card-body">
+                    <div className="card-text text-center w-100">
+                        <p className="w-100 text-prime border-bottom h4 website-name">
+                            {project.title}
+                        </p>
+                        <p className="w-100 text-light-second technology">
+                            {project.tech}
+                        </p>
+                        {project.gitHubLink? <a className="btn btn-lg btn-block my-1" href={project.gitHubLink} target='_blank' role="button">View GitHub</a> : <br></br>}
+                        {project.demoLink? <a className="btn btn-lg btn-block my-1" href={project.demoLink} target='_blank' role="button">Live Demo</a> : <br></br>}
+                    </div>
+
+            </div>
+
+            {/* <h2>{project.title}</h2>
             <h3>{project.demoLink}</h3>
-            <h3>{project.gitHubLink}</h3>
+            <h3>{project.gitHubLink}</h3> */}
+
 
         </div>
+
+
     );
 }
 export default Project;
