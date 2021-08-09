8260f18a99d07537505b9f5d942f426a8c7b17c7
Molly Wilson <molly_wilson1@outlook.com>
2021-08-08 20:23:10 -0500

    form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,68 @@
+import { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 
 function Contact() {
+
+    const [formState, setFormState] = useState({ first: '', last: '', email: '', message: '' });
+    const { first, last, email, message } = formState;
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'Email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value });
+            console.log('Form', formState);
+          }
+    }
+
     return (
         <>
-            <form className="row g-3 p-5">
+            <form className="row g-3 p-5" onSubmit={handleSubmit}>
             <h2>Contact Me</h2>
                 <div className="col-md-6">
-                    <label for="inputFirst" className="form-label">First</label>
-                    <input type="name" className="form-control" id="inputFirst" placeholder="Joe"></input>
+                    <label htmlFor='First name' className="form-label">First</label>
+                    <input type="text" defaultValue={first} className="form-control" onBlur={handleChange} name='First name' required></input>
                 </div>
                 <div className="col-md-6">
-                    <label for="inputLast" className="form-label">Last</label>
-                    <input type="name" className="form-control" id="inputLast" placeholder="Smith"></input>
+                    <label htmlFor='Last name' className="form-label">Last</label>
+                    <input type="text" defaultValue={last} className="form-control" onBlur={handleChange} name='Last name' required></input>
                 </div>
                 <div className="col-12">
-                    <label for="inputEmail" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail" placeholder="joe.smith@email.com"></input>
+                    <label htmlFor='Email' className="form-label">Email</label>
+                    <input type="email" defaultValue={email} className="form-control" onBlur={handleChange} name='Email' required></input>
                 </div>
                 <div className="col-12">
-                    <label for="inputMessage" className="form-label">Message</label>
-                    <textarea type="Message" className="form-control" id="inputMessage" placeholder="Great work!"></textarea>
+                    <label htmlFor='Message' className="form-label">Message</label>
+                    <textarea defaultValue={message} className="form-control" onBlur={handleChange} name='Message' required></textarea>
                 </div>
+
+                {errorMessage && (
+                    <div>
+                        <p className="error">{errorMessage}</p>
+                    </div>
+                )}
 
                 <div className="col-12">
                     <button type="submit" className="btn btn-lg btn-block">Send</button>
